cda2ee98a36984e1b8fb2719445077674d5e4905
ryuken73 <ryuken73@gmail.com>
2022-09-11 15:27:51 +0900

    etc: examine draw module(perfect freehand)

--- src/renderer/Components/Draw/DrawSvg.jsx
+++ src/renderer/Components/Draw/DrawSvg.jsx
@@ -14,7 +14,7 @@ const StyledSvg = styled.svg`
 `
 
 const options = {
-  size: 10,
+  size: 14,
   thinning: 0.5,
   smoothing: 0.5,
   streamline: 0.5,
@@ -25,20 +25,23 @@ const options = {
     cap: true,
   },
   end: {
-    taper: 0,
+    taper: 1,
     easing: (t) => t,
     cap: true,
   },
 };
 
 const DrawSvg = (props) => {
-  // const [pathDatum, setPathDatum] = React.useState([]);
-  const { pathDatum, setPathDatumState } = useDrawState();
+  const {pathDatum, addPathDatumState} = useDrawState();
   const [points, setPoints] = React.useState([]);
+  const [mouseUp, setMouseUP] = React.useState(false);
+
   const pathData = React.useRef([]);
+  const rawPathData = React.useRef([]);
 
   function handlePointerDown(e) {
     e.target.setPointerCapture(e.pointerId);
+    setMouseUP(false);
     setPoints([[e.pageX, e.pageY, e.pressure]]);
   }
 
@@ -48,12 +51,19 @@ const DrawSvg = (props) => {
   }
 
   const handlePointerUp = React.useCallback(() => {
-    setPathDatumState(pathData.current);
-  }, [setPathDatumState]);
+    setMouseUP(true);
+    addPathDatumState(pathData.current);
+  }, [addPathDatumState]);
 
   const stroke = getStroke(points, options);
   // const pathData = getSvgPathFromStroke(stroke);
   pathData.current = getSvgPathFromStroke(stroke);
+  // rawPathData.current = getSvgPathFromStroke(points);
+
+  // console.log(pathData.current);
+  // console.log(rawPathData.current);
+  // console.log(stroke);
+  // console.log(points);
 
   return <StyledSvg
       onPointerDown={handlePointerDown}
@@ -61,10 +71,33 @@ const DrawSvg = (props) => {
       onPointerUp={handlePointerUp}
       style={{ touchAction: 'none' }}
     >
-      {pathDatum.map((pathData) => (
-        <path d={pathData} />
+      {/* <marker id="triangle" viewBox="0 0 10 10"
+            refX="1" refY="5"
+            markerUnits="strokeWidth"
+            markerWidth="10" markerHeight="10"
+            orient="auto">
+        <path d="M 0 0 L 10 5 L 0 10 z" fill="#f00"/>
+      </marker> */}
+      {pathDatum.map(pathData => (
+        <path
+          d={pathData}
+          fill={'white'}
+          stroke="black"
+          strokeWidth={'3'}
+        />
       ))}
-      {points && <path d={pathData.current} />}
+      {points && !mouseUp && (
+        // <path d={pathData.current} marker-end="url(#triangle)"></path>
+        // <path d={pathData.current} markerEnd={mouseUp && "url(#triangle)"}></path>
+        <path
+          d={pathData.current}
+          fill={'white'}
+          stroke="black"
+          strokeWidth={'3'}
+          // strokeLinejoin={'square'}
+          // strokeLinecap={'square'}
+        ></path>
+      )}
     </StyledSvg>
 };
 
