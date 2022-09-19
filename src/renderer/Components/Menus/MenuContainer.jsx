cf358773a9376a603e8c70202a3e409db9a03981
ryuken73 <ryuken73@gmail.com>
2022-09-19 23:22:20 +0900

    update: attach handler before MenuContainer

--- src/renderer/Components/Menus/MenuContainer.jsx
+++ src/renderer/Components/Menus/MenuContainer.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import Draggable from 'react-draggable';
+import DragHandle from 'renderer/Components/Draw/DragHandle';
+import useSyncPosition from 'renderer/hooks/useSyncPosition';
 import useAssetState from 'renderer/hooks/useAssetState';
 import CONSTANTS from 'renderer/config/constants';
 import MenuItem from './MenuItem';
 
 const { POSITION } = CONSTANTS;
-
-const Container = styled.div`
+const MenuDiv = styled.div`
   position: absolute;
   top: ${POSITION.menuContainer.top};
   right: ${POSITION.menuContainer.right};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  z-index: 9999;
+`
+
+const Container = styled.div`
+  /* position: absolute; */
+  /* top: ${POSITION.menuContainer.top}; */
+  /* right: ${POSITION.menuContainer.right}; */
   display: ${(props) => (props.hide ? 'none' : 'flex')};
   flex-direction: row;
   z-index: 9999;
@@ -22,30 +34,47 @@ const Container = styled.div`
   padding: 5px;
   /* width: 80%; */
   justify-content: space-around;
+  align-items: center;
   border-radius: 20px;
   /* border-bottom: 6px solid white; */
 `;
 
+const DivDragHandler = styled.div`
+  display: ${(props) => props.hide && 'none'};
+  opacity: 0.8;
+`
+
 const MenuContainer = (props) => {
   // eslint-disable-next-line react/prop-types
   const { drawShow } = props;
+  const { position, syncPosition } = useSyncPosition();
   const { assets, currentAsset, setCurrentAssetState } = useAssetState();
   return (
-    <Draggable>
-      <Container hide={drawShow}>
-        {assets.map((asset, index) => (
-          // eslint-disable-next-line react/button-has-type
-          <MenuItem
-            key={asset.title}
-            isCurrent={currentAsset === index}
-            menuText={asset.title}
-            onClick={() => {
-              setCurrentAssetState(index);
-            }}
-          />
-        ))}
-      </Container>
-    </Draggable>
+    <MenuDiv>
+      <Draggable onDrag={syncPosition}>
+        <DivDragHandler hide={drawShow}>
+          <DragHandle size="small" />
+        </DivDragHandler>
+      </Draggable>
+      <Draggable position={position} onStart={() => false}>
+        <Container hide={drawShow}>
+          {assets.map((asset, index) => (
+            // eslint-disable-next-line react/button-has-type
+            <MenuItem
+              key={asset.title}
+              isCurrent={currentAsset === index}
+              menuText={asset.title}
+              onClick={() => {
+                setCurrentAssetState(index);
+              }}
+              onTouchStart={() => {
+                setCurrentAssetState(index);
+              }}
+            />
+          ))}
+        </Container>
+      </Draggable>
+    </MenuDiv>
   );
 };
 
