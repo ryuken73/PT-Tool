c6239a8c8766e56621b5eed0dbbd3c57d41f5a62
ryuken73 <ryuken73@gmail.com>
2023-01-23 17:34:57 +0900

    refactor: remove unused

--- src/renderer/Components/Assets/Asset.jsx
+++ src/renderer/Components/Assets/Asset.jsx
@@ -10,16 +10,6 @@ const Container = styled.div`
   display: ${(props) => (props.show ? 'block' : 'none')};
   overflow: hidden;
   transition: transform 0.5s;
-  /* height: ${(props) => (props.show ? '100%' : '0%')}; */
-  /* width: ${(props) => (props.show ? '100%' : '0%')}; */
-  /* visibility: ${(props) => (props.show ? 'visible' : 'hidden')}; */
-  /* width: ${(props) => (props.show ? '100%' : '0%')}; */
-  /* transition: all 1s; */
-  /* visibility: ${(props) => (props.show ? 'visible' : 'hidden')}; */
-  /* filter: grayscale(100%); */
-  /* filter: ${(props) => props.drawOn && 'grayscale(100%)'}; */
-  /* filter: ${(props) => props.drawOn && 'contrast(175%) brightness(103%)'}; */
-  /* ${(props) => props.drawOn && 'transform: scale(1.01)'}; */
 `;
 
 const AssetContainer = (props) => {
